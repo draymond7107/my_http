4a9921741958016778295a2e0eeec91236ba7a65
张苏超 <draymond7107@sina.com>
2020-01-06 17:36:35 +0800

    第一次提交

---
+++ src/test/java/com/draymond/my_http/MyHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.draymond.my_http;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHttpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
